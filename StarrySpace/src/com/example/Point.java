d153b52fb2ce6d004703846831b9fa067c417239
Semenko Olexander <green.honey.66@gmail.com>
2020-11-21 19:23:28 +0200

    Starry space

---
+++ StarrySpace/src/com/example/Point.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+public class Point {
+    float x;
+    float y;
+    float z;
+
+    public Point(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
